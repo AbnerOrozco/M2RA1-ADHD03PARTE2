cb13edd7707dc5d686544ad4ac17a70d451fa9b7
JoselynMMM <87822810+JoselynMMM@users.noreply.github.com>
2024-05-15 12:42:44 -0600

    actividad 2 finalizada

---
+++ problema1.js
@@ -0,0 +1,36 @@
+function operacionSeleccionada(operacion) {
+    const numero1 = parseFloat($("#numero1").val());
+    const numero2 = parseFloat($("#numero2").val());
+    const numero3 = parseFloat($("#numero3").val());
+    const numero4 = parseFloat($("#numero4").val());
+  
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3) || isNaN(numero4)) {
+      alert("Por favor, introduzca solo números válidos.");
+      return;
+    }
+  
+    let resultado = "";
+    switch(operacion) {
+        case "promedio":
+            const promedio = (numero1 + numero2 + numero3 + numero4) / 4;
+            resultado = `Promedio: ${promedio.toFixed(2)}`;
+            break;
+        case "indicarValores":
+            if (numero1 === numero2 && numero2 === numero3 && numero3 === numero4) {
+                resultado = "Todos los números son iguales";
+            } else {
+                resultado = "Los números son diferentes";
+            }
+            break;
+        case "valorMayor":
+            let valorMayor = Math.max(numero1, numero2, numero3, numero4);
+            resultado = `El valor mayor es: ${valorMayor}`;
+            break;
+        default:
+            alert("Operación no válida");
+            return;
+    }
+  
+    $("#resultado").text(resultado);
+    $("#txt-resultado").hide(); // Oculta el elemento con ID txt-resultado
+}
